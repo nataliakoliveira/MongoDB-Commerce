e9cb0feb1d0506127805809b9eb8b06e9b9cd054
nataliakoliveira <nataliaakellyy@gmail.com>
2023-03-15 21:01:29 -0300

    Adicione o valor muito sódio ao final do array tags nos produtos em que o percentual de sódio seja maior ou igual a 40

---
+++ challenges/desafio25.js
@@ -0,0 +1,5 @@
+db.produtos.updateMany(
+  { "valoresNutricionais.2.tipo": "sódio", "valoresNutricionais.2.percentual": { $gt: 40 } },
+  { $addToSet: { tags: "muito sódio" } },
+);
+db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
